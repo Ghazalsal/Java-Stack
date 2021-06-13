addb97691f1feeb9612e86e690f8c168520fb33f
Ghazal Salameh <g_antar_n@hotmail.com>
2021-06-13 15:51:25 +0300

    Object master 1 solution

---
+++ Java/Java-OOP/Object-Master-1/Human.java
@@ -0,0 +1,13 @@
+
+public class Human {
+	int strength=3;
+    int stealth=3;
+    int intelligence=3;
+    int health =100;
+
+    public int attack(Human save) {
+        save.health -=this.strength;
+        return save.health;
+
+    }
+}
---
+++ Java/Java-OOP/Object-Master-1/HumanTest.java
@@ -0,0 +1,12 @@
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		Human newHuman = new Human();
+		Human ghazal = new Human();
+		
+		int x= ghazal.attack(newHuman);
+		System.out.println(x);
+	}
+
+}
